b703c1a9a71b9a44cfac6b814dd4645a44dd4de3
Joseph Dye <josephpdye@icloud.com>
2025-09-28 15:40:20 -0400

    Finished first round of tests

--- PracticeRooms/FlaskApp/tests/test_createRooms.py
+++ PracticeRooms/FlaskApp/tests/test_createRooms.py
@@ -1,4 +1,4 @@
-from practiceRooms import MyRoom, createRooms, updateRoom
+from FlaskApp.practiceRooms import MyRoom, createRooms, updateRoom
 from datetime import datetime, timezone
 from unittest.mock import patch
 
@@ -10,14 +10,19 @@ def test_createRooms():
     with patch("FlaskApp.practiceRooms.datetime") as mock_datetime:
         mock_datetime.now.return_value = fake_now
         
-        for i in range (0, 8):
+        for i in range (0, 9):
+            assert rooms[i].id == i+1
+            assert rooms[i].occupied == 0
             updateRoom(rooms[i])
-            assert rooms[i].id == i
+            assert rooms[i].id == i+1
             assert rooms[i].occupied == 1
             assert rooms[i].in_use == fake_now
 
-        for i in range (0, 8, -1):
+        for i in range (8, -1, -1):
+            assert rooms[i].id == i+1
+            assert rooms[i].occupied == 1
+            assert rooms[i].in_use == fake_now
             updateRoom(rooms[i])
-            assert rooms[i].id == i
+            assert rooms[i].id == i+1
             assert rooms[i].occupied == 0
             assert rooms[i].in_use == fake_now
