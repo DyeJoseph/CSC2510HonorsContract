38a4097d11f7ccc397bc9386cc5eb68cda5b0af9
Joseph Dye <josephpdye@icloud.com>
2025-10-29 00:50:26 -0500

    Edit for GCP Run

--- PracticeRooms/practiceRooms.py
+++ PracticeRooms/practiceRooms.py
@@ -82,7 +82,7 @@ def updateRoom(room):
     socketio.emit('refresh')
     
 #Run
-if __name__ in "__main__":
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     socketio.run(app, host="0.0.0.0", port=8080)
