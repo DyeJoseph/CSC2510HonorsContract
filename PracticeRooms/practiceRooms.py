c6905d1af5f5f3d563818634e2f6594253df56da
Joseph Dye <josephpdye@icloud.com>
2025-11-13 21:08:59 -0600

    Edited database to not be a global variable. Uses SQL instead

--- PracticeRooms/practiceRooms.py
+++ PracticeRooms/practiceRooms.py
@@ -15,44 +15,26 @@ socketio = SocketIO(app)
 app.secret_key = "123456"
 Scss(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://username:password@localhost:5432/dbname"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy(app)
 
 #MyRoom class
 class MyRoom(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     occupied = db.Column(db.Integer,default = 0)
-    in_use = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    in_use = db.Column(db.DateTime, default=datetime.now())
 
     def __repr__(self) -> str:
         return f"Task {self.id}"
     
 def createRooms():
-    #Array for rooms
-    rooms = []
-
-    #Room initialization and append to array
-    room1 = MyRoom(id=1, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room1)
-    room2 = MyRoom(id=2, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room2)
-    room3 = MyRoom(id=3, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room3)
-    room4 = MyRoom(id=4, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room4)
-    room5 = MyRoom(id=5, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room5)
-    room6 = MyRoom(id=6, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room6)
-    room7 = MyRoom(id=7, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room7)
-    room8 = MyRoom(id=8, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room8)
-    room9 = MyRoom(id=9, occupied=0, in_use=datetime.now(timezone.utc))
-    rooms.append(room9)
-    return rooms
-    
-rooms = createRooms()
+    if MyRoom.query.count() == 0:
+        for i in range(1, 10):
+            room = MyRoom(id=i, occupied=0, in_use=datetime.now())
+            db.session.add(room)
+        db.session.commit()
 
 #Home page
 @app.route("/",methods=["POST","GET"])
@@ -60,16 +42,19 @@ def index():
     #Toggle room availability
 
     if request.method == "POST":
-        room_index = int(request.form["room"])
-        updateRoom(rooms[room_index])
-
+        room_index = int(request.form["room"]) + 1
+        with app.app_context():
+            room = db.session.get(MyRoom, room_index)
+            updateRoom(room)
+    rooms = MyRoom.query.order_by(MyRoom.id).all()
     return render_template("index.html", rooms=rooms)
 
 #Details page (shows how long the room has been in use)
 @app.route("/details/room<int:id>")
 def details(id):
+    rooms = MyRoom.query.order_by(MyRoom.id).all()
     room_data = rooms[id-1]
-    elapsed_time = datetime.now(timezone.utc) - rooms[id-1].in_use
+    elapsed_time = datetime.now() - rooms[id-1].in_use
     return render_template('details.html', room_data=room_data, elapsed_time=elapsed_time)
 
 #Function to update room availability
@@ -80,13 +65,17 @@ def updateRoom(room):
     #If room is vacant, set it to occupied and start timer
     else:
         room.occupied = 1
-        room.in_use = datetime.now(timezone.utc)
+        room.in_use = datetime.now()
         #Comment out the line below this if running tests
         # flash(f"Room {room.id} is now in use!")
+    db.session.commit()
     socketio.emit('refresh')
     
 #Run
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+        createRooms()
+    # socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+    socketio.run(app, host="0.0.0.0", port=8080)
+    # app.run(host="0.0.0.0", port=8080)
